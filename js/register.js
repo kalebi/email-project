abd1823ae0fca3573a50f46ff6da133d10a69c96
Leyukarii <55262488+Leyukarii@users.noreply.github.com>
2019-11-19 17:24:50 -0300

    Arrumando o form

--- js/register.js
+++ js/register.js
@@ -1,57 +1,57 @@
-$(document).ready(function(){
-    $("input").attr("required",true);
+$(document).ready(function () {
+    $("input").attr("required", true);
 
-    $("formId#formId").submit(function(){
+    $("formId#formId").submit(function () {
         preenchido = true;
-        $("input").each(function(){ //percorrer todos os inputs para verificar se todos estão preenchidos 
-            if($(input).val()==""){ 
+        $("input").each(function () { //percorrer todos os inputs para verificar se todos estão preenchidos 
+            if ($(input).val() == "") {
                 preenchido = false;
             }
         });
 
         //verifica se a senha é igual ao confirmar senha
-        if(preenchido){
-            if($.trim($("#createSenha").val()==$.trim($("#confirmSenha").val()))){
+        if (preenchido) {
+            if ($.trim($("#createSenha").val() == $.trim($("#confirmSenha").val()))) {
                 $.ajax({
                     type: "POST",
                     dataType: "json",
-                    url:"../php/register.php",
-                    data:{
+                    url: "../php/register.php",
+                    data: {
                         "createNome": $("#createNome").val(),
                         "createSobrenome": $("#createSobrenome").val(),
                         "createEmail": $("#createEmail").val(),
                         "createSenha": $("#createSenha").val(),
                         "confirmSenha": $("#confirmSenha").val()
                     },
-                    success:function(retorno){
-                        switch(retorno){
-                            case 1: 
+                    success: function (retorno) {
+                        switch (retorno) {
+                            case 1:
                                 alert("Usuário cadastrado com sucesso!");
-                                window.location.href="login.html";
+                                window.location.href = "login.html";
                                 break;
-                            
-                            case 2: 
+
+                            case 2:
                                 alert("Esse usuário já está cadastrado!");
                                 break;
 
-                            case 3: 
+                            case 3:
                                 alert("Sem permissão");
                                 break;
                         }
                     },
 
-                    error:function(){
+                    error: function () {
                         alert("Sem conexão ao servidor!");
                     }
                 });
-            }else{
+            } else {
                 alert("Por favor preencha todos os campos");
             }
             return false;
         }
     });
-    
-    $("#bGoToLogin").click(function(){
-        window.location.href="login.html";
+
+    $("#bGoToLogin").click(function () {
+        window.location.href = "login.html";
     });
 });
