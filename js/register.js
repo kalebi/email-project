2337fd0df3dab6d2479568a3bfff4c224bf8ad8e
Leyukarii <55262488+Leyukarii@users.noreply.github.com>
2019-11-20 01:09:10 -0300

    a

--- js/register.js
+++ js/register.js
@@ -1,30 +1,28 @@
 $(document).ready(function(){
-    $("#bEntrar").click()
-            if ($.trim($("#createSenha").val() == $.trim($("#confirmSenha").val()))) {
-                var ajax_nome = $("#createNome").val();
-                var ajax_sobrenome = $("#createSobrenome").val();
-                var ajax_email = $("#createEmail").val();
-                var ajax_senha = $("#createSenha").val();
-                var ajax_confirmsenha = $("#confirmSenha").val();
-
-                $.ajax({
-
-                    type: "POST",
-                    url: "../php/register.php",
-                    data: {
-                        nome = ajax_nome,
-                        sobrenome = ajax_sobrenome,
-                        email = ajax_email,
-                        senha = ajax_senha,
-                        confimar = ajax_confirmsenha
-                    },
-                    success: function(retorno){
-                        alert(retorno);
-                    }
-                });
+    $("#bEntrar").click(function(){
+        var ajax_nome = $("#createNome").val();
+        var ajax_sobrenome = $("#createSobrenome").val();
+        var ajax_email = $("#createEmail").val();
+        var ajax_senha = $("#createSenha").val();
+        var ajax_confirmsenha = $("#confirmSenha").val();
+            
+        $.ajax({
+                
+            type: "POST",
+            url: "../php/register.php",
+            data: {
+                nome = ajax_nome,
+                sobrenome = ajax_sobrenome,
+                email = ajax_email,
+                senha = ajax_senha,
+                confimar = ajax_confirmsenha
+            },
+            success: function(retorno){
+                alert(retorno);
             }
-        }
+        });
     });
+
 
     $("#bGoToLogin").click(function () {
         window.location.href = "login.html";
